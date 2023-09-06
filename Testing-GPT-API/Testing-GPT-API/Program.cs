8deaa81db27c0f66f288c27465a695a7db24ca6f
JokoLG <107288693+JokoLG@users.noreply.github.com>
2023-09-06 16:57:15 -0400

    asdsadasdad

---
+++ Testing-GPT-API/Testing-GPT-API/Program.cs
@@ -0,0 +1,61 @@
+﻿using OpenAI.GPT3;
+using OpenAI.GPT3.Interfaces;
+using OpenAI.GPT3.Managers;
+using OpenAI.GPT3.ObjectModels.RequestModels;
+using OpenAI.GPT3.ObjectModels;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Testing_GPT_API
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            String UserInput;
+
+            var apiKey = "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";
+            var gpt3 = new OpenAIService(new OpenAiOptions() {ApiKey = apiKey});
+
+            Console.WriteLine("Insert Prompt: ");
+            UserInput = Console.ReadLine();
+
+            /*
+            var response = openAi.Image.create(
+                prompt = UserInput,
+                nameof = 1,
+                size="256x256"
+            );
+            image_url = response['data'][0]['url'];
+            Console.WriteLine(image_url);
+            */
+
+            var completionResult = await gpt3.Completions.CreateCompletion(new CompletionCreateRequest()
+            {
+                Prompt = UserInput,
+                Model = Models.TextDavinciV2,
+                Temperature = 0.5F,
+                MaxTokens = 100,
+                N = 1
+            });
+            
+
+            if (completionResult.Successful)
+            {
+                foreach (var choice in completionResult.Choices)
+                {
+                    Console.WriteLine(choice.Text);
+                }
+            }
+            else
+            {
+                if (completionResult.Error == null)
+                {
+                    throw new Exception("Unknown Error");
+                }
+                Console.WriteLine($"{completionResult.Error.Code}: {completionResult.Error.Message}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TheBestAPI/TheBestAPI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using NHentaiSharp.Core;
+using NHentaiSharp.Search;
+
+string[] tags = new[] { // Tags for the upcoming searches
+  "loli", // We add the tag loli
+  NHentaiSharp.Core.SearchClient.GetExcludeTag("rape"), // We exclude the tag rape
+  NHentaiSharp.Core.SearchClient.GetCategoryTag("kantai collection", NHentaiSharp.Search.TagType.Parody)
+  // We search doujinshi of the anime/game Kantai Collection
+};
+
+Random r = new Random();
+// We do a search with the tags
+var result = await NHentaiSharp.Core.SearchClient.SearchWithTagsAsync(tags);
+int page = r.Next(0, result.numPages) + 1; // Page count begin at 1
+// We do a new search at a random page
+result = await NHentaiSharp.Core.SearchClient.SearchWithTagsAsync(tags, page);
+var doujinshi = result.elements[r.Next(0, result.elements.Length)]; // We get a random doujinshi
+
+Console.WriteLine("Random doujinshi: " + doujinshi.prettyTitle + Environment.NewLine +
+  "URL: " + doujinshi.url);
