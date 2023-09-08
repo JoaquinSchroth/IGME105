a1651492520e2c65267033661325d7964ec0d950
JoaquinSchroth <143818932+JoaquinSchroth@users.noreply.github.com>
2023-09-08 10:53:43 -0400

    asdasdas

---
+++ TestingStuff/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TestingStuff
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A " + 4 + " x " + 5 + " rectangle:");
+            Console.WriteLine("Area: " + (4 * 5));
+            Console.WriteLine("Perimeter: " + ((4 * 2) + (5 * 2)));
+            Console.WriteLine("Perimeter: " + (4 * 2 + 5 * 2));
+        }
+    }
+}
