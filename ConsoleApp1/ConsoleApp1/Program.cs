ce54f862343b206330ae070a26d9a4911ee13f0c
JokoLG <107288693+JokoLG@users.noreply.github.com>
2023-09-09 23:37:00 -0400

    ayuda

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int x = 1;
+            while(x == 1)
+            {
+                Console.WriteLine("aaa");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
