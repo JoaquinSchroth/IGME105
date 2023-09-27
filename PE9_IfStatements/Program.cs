f5a1b020c2b41637044bca6e22818ae17b9165bb
JokoLG <107288693+JokoLG@users.noreply.github.com>
2023-09-27 01:57:03 -0400

    PE 9

---
+++ PE9_IfStatements/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PE9_IfStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
